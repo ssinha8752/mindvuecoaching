1739e6befee18d599d47dd2622fec0993893d8b1
ssinha8752 <shubhamsinha8752@yahoo.com>
2021-12-09 01:39:28 +0530

    [Shubham] Adding all the data for tiles

---
+++ src/components/Home/components/Content/data/ContentData.js
@@ -0,0 +1,40 @@
+import Heart from '../images/heart2.jpg'
+import Thought from '../images/Thought.jpg'
+import Soul from '../images/Soul.jpg'
+import Health from '../images/Health.jpg'
+
+export const CONTENT ={
+    head:"“All that we are is the result of what we have thought. The mind is everything. What we think is what we become.” – Buddha",
+    p1:"At MindVue, we believe in creating a safe space for you which involves exploring the subconscious mind, uncovering your purpose, connecting to your core values and provoking fresh perspectives.",
+    p2:"We create this safe space by designing introspective, innovative, interesting coaching & training workshops which focuses on four fundamental principles."
+}
+
+export const TILES = {
+    head:"PRINCIPLES OF COACHING AND TRAINING",
+    tiles:[
+        {
+            pic:Thought,
+            key:"Thought",
+            value:"Thoughts are things. They are powerful. They are preemptive. The very thoughts running through your mind can help to manifest the life of your dreams or draw you closer to your greatest fears",
+            head:"THOUGHT VUE"
+        },
+        {
+            pic:Heart,
+            key:"Heart",
+            value:"Emotions determine the quality of our lives. We dont have much control over what we become emotional about, but it is possible to make some changes in what triggers our emotions and how we behave when we are emotional.",
+            head:"HEART VUE"
+        },
+        {
+            pic:Soul,
+            key:"Soul",
+            value:"It relates to the connection of that we have with our soul. It is about feeling the joy, satisfaction and warm feeling in your heart and gut.",
+            head:"SOUL VUE"
+        },
+        {
+            pic:Health,
+            key:"Health",
+            value:"Our Health is at the center of our life. Every part of our life relies on us having good health. even relatively minor health issues such as aches, pains, lethargy and indigestion take a toll on our happiness and stress levels.",
+            head:"HEALTH VUE"
+        }
+    ]
+}
