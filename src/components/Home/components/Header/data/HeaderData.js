50c9e61072bd289c7f5f8579b7cadfb8f3c05e34
ssinha8752 <shubhamsinha8752@yahoo.com>
2021-12-08 16:23:52 +0530

    [Shubham] Adding the data for Header Menu as JSON

---
+++ src/components/Home/components/Header/data/HeaderData.js
@@ -0,0 +1,104 @@
+export const ABOUT_ME = [
+    {
+        key:"aboutMe1",
+        value:"My Story"
+    },
+    {
+        key:"aboutMe2",
+        value:"Associations"
+    },
+    {
+        key:"aboutMe3",
+        value:"Testimonials"
+    },
+]
+
+export const MIND_CATALOGUE=[
+    {
+        key:"MindCatalogue:1",
+        value:"Executive coaching"
+    },
+    {
+        key:"MindCatalogue:2",
+        value:"Life Coaching"
+    },
+    {
+        key:"MindCatalogue:3",
+        value:"Youth Coaching"
+    },
+    {
+        key:"MindCatalogue:4",
+        value:"Trainings and Workshops"
+    }
+]
+
+export const MIND_AND_MATTER=[
+    {
+        key:"MindAndMatter:1",
+        value:"Articles"
+    },
+    {
+        key:"MindAndMatter:2",
+        value:"Quotes"
+    },
+    {
+        key:"MindAndMatter:3",
+        value:"Doodles"
+    }
+]
+
+export const PODCAST=[
+    {
+        title:"Podcast #1",
+        key:"Podcast:1",
+        value:"Ozlem Soriagolu"
+    },
+    {
+        title:"Podcast #2",
+        key:"Podcast:2",
+        value:"Anurag Kapoor"
+    },
+    {
+        title:"Podcast #3",
+        key:"Podcast:3",
+        value:"Prajna Paramite"
+    },
+    {
+        title:"Podcast #4",
+        key:"Podcast:4",
+        value:"Harsh Kumar Nangea"
+    },
+    {
+        title:"Podcast #5",
+        key:"Podcast:5",
+        value:"Dr.Meenal Chauhan"
+    }
+]
+
+export const ILLUMINATED_MIND = [
+    {
+        key:"illuminatedmind1",
+        value:"What is Coaching ?"
+    },
+    {
+        key:"illuminatedmind2",
+        value:"NLP"
+    },
+    {
+        key:"illuminatedmind3",
+        value:"Mindfulness"
+    },
+    {
+        key:"illuminatedmind4",
+        value:"Cognitive Behavior"
+    },
+    {
+        key:"illuminatedmind5",
+        value:"Positive Psychology"
+    },
+    {
+        key:"illuminatedmind6",
+        value:"Art of Coaching"
+    }
+
+]
