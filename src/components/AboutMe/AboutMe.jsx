9f6cef181e12c853d462eca43158284b996a1aba
ssinha8752 <shubhamsinha8752@yahoo.com>
2021-12-10 01:29:52 +0530

    [Shubham] Adding associations in the AboutMe page

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import MyStory from './components/MyStory/MyStory';
 import './AboutMe.css'
 import Testimonials from './components/Testimonials/Testimonials';
+import Associations from '../Assoications/Associations';
 
 class AboutMe extends Component{
     render(){
         return <div className="AboutMe_container">
             <div><MyStory/></div>
             <div><Testimonials/></div>
-            <div>Associations</div>
+            <div><Associations/></div>
         </div>
     }
 }
