dce0d876bf685f5ab642e8a1be6db1375af92cfe
ssinha8752 <shubhamsinha8752@yahoo.com>
2021-12-10 21:36:44 +0530

    [Shubham] Editing header for MyStory

--- src/components/AboutMe/components/MyStory/MyStory.jsx
+++ src/components/AboutMe/components/MyStory/MyStory.jsx
@@ -4,7 +4,8 @@ import './MyStory.css'
 
 class MyStory extends Component{
     render(){
-        return <div>
+        return <div className='mystory_container'>
+            <h1 style={{textAlign:"left", fontSize:'35px', marginTop:'20px', fontWeight:'bolder'}}>MY STORY</h1>
             {
                 MY_STORY.map(((story)=>
                     <div className="mystory">
