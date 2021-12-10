cee0ec3d65535076862f514dd0427b7d413244ca
ssinha8752 <shubhamsinha8752@yahoo.com>
2021-12-10 21:36:02 +0530

    [Shubham] Editing Testimonials to show carousel

--- src/components/AboutMe/components/Testimonials/Testimonials.jsx
+++ src/components/AboutMe/components/Testimonials/Testimonials.jsx
@@ -5,54 +5,23 @@ import './Testimonials.css'
 
 class Testimonials extends Component{
     render(){
-        // return <div className="testimonials">
-        //     {
-        //         TESTIMONIALS.map(((testimonial, index)=>
-        //             (index%2===0)?
-        //             <div className="tiles">
-        //                 <div className="card">
-        //                     <h2>{testimonial.heading}</h2>
-        //                     <h1>{testimonial.subheading}</h1>
-        //                     <p>{testimonial.value}</p>
-        //                 </div>
-        //                 <div className="card_pic">
-        //                         <img src={testimonial.pic} alt="" style={{width:"100%"}}/>
-        //                 </div>
-        //             </div>:
-        //             <div className="tiles">
-        //                 <div className="card_pic">
-        //                     <img src={testimonial.pic} alt="" style={{width:"100%"}}/>
-        //                 </div>
-        //                 <div className="card">
-        //                     <h2>{testimonial.heading}</h2>
-        //                     <h1>{testimonial.subheading}</h1>
-        //                     <p>{testimonial.value}</p>
-        //                 </div>
-        //             </div>
-        //         ))
-        //     }
-        // </div>
-        const contentStyle = {
-            height: '600px',
-            color: '#fff',
-            lineHeight: '160px',
-            textAlign: 'center',
-            background: '#242B2E',
-        };
-        return <Carousel autoplay>
-            <div>
-      <h3 style={contentStyle}>1</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>2</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>3</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>4</h3>
-    </div>
+        return <div>
+        <h1 style={{textAlign:"left", fontSize:'35px', marginTop:'20px', fontWeight:'bolder'}}>HEAR FROM OUR CLIENTS : Building Trust & Inspiring Individuals</h1>
+        <Carousel autoplay>
+          {
+            TESTIMONIALS.map(((testimonial)=>
+                <div>
+                  <div className='carousel_card'>
+                <h1>{testimonial.heading}</h1>
+                <h2>{testimonial.subheading}</h2>
+                <p>{testimonial.value}</p>
+                </div>
+                <img src={testimonial.pic} alt="" style={{width:"100%"}}/>
+                </div>
+            ))
+          }
         </Carousel>
+        </div>
     }
 }
 
--- src/components/AboutMe/components/Testimonials/TestimonialsData.js
+++ src/components/AboutMe/components/Testimonials/TestimonialsData.js
@@ -1,34 +1,46 @@
-import Pic from '../Testimonials/Pic.jpg'
+import KP from './TestimonialsImages/KP.jpg'
+import VP from './TestimonialsImages/VP.jpg'
+import PSP from './TestimonialsImages/PSP.jpg'
+import VD from './TestimonialsImages/VD.jpg'
+import DG from './TestimonialsImages/DG.jpg'
+import NG from './TestimonialsImages/NG.jpg'
+import youth from './TestimonialsImages/youth.jpg'
 
 export const TESTIMONIALS =[
     {
-        pic:Pic,
+        pic:KP,
         heading:"K.P SINGH",
         subheading:"EX. JOINT PRESIDENT, JAYPEE CEMENTS, MADHYA PRADESH",
         value:"I participated in a webinar recently conducted by mindfulness and enjoyed it from the starting. The method through which Gunjan explain such an intricate subject motivated me to attempt practicing the mindfulness. "
     },
     {
+        pic:VP,
         heading:"VICE PRESIDENT",
-        subheading:"LEADING INVESTMENT BANK PUNE",
+        subheading:"LEADING INVESTMENT BANK, PUNE",
         value:"Thanks Gunjan for helping me prioritise and align my goals, and for actually helping me identify the right goals too. I love the positivity and calming energy you bring into our sessions, I appreciate your simple and super-patient approach, and I look forward to the new tools and techniques that you bring in week on week. I feel I am little more positive than yesterday and I do believe we can breakdown all obstacles into small chunks and tackle with faith, plan and commitment. Thank you!"
     },
     {
+        pic:NG,
         heading:"NEHA GINORIA",
         subheading:"HEAD OF MARKETING, EREVMAX KOLKATA",
         value:"The webinar was informative and useful, specially in today's time when our resiliency is being tested often, by circumstances around us. Gunjan is a subject matter expert and her delivery style is unique. Since the session had exercises for the audience, it was engaging and helped me to understand certain concepts with ease. Overall the session was awesome. It helped me go deeper into my personality and uncover couple of layers to understand myself a little better - that was my biggest takeaway!"
     },{
+        pic:PSP,
         heading:"PARTHA SARATHI SARKAR",
         subheading:"HEAD TECHNOLOGY GROUP THEIS PRECISION STEEL INDIA, GUJRAT",
         value:"I had the pleasure of attending the webinar, which was hosted by Gunjan Sinha. She has an excellent ability to handle sessions where people from different fields come and participate. would have high recommendation for her eloquent speech & ability to interpret complex cases easily. "
     },{
+        pic:DG,
         heading:"DEEPTI GUPTA",
-        subheading:"DATA SCIENTIST, SCHNEIDER ELECTRIC BENGALURU",
-        value:"I have been connected with Gunjan for quite a few months now, my interaction with her has always been empowering and positive. Her commitment, eagerness to deliver, and continuous learning makes her stand apart from the crowd. She has capabilities and discipline to use her knowledge at the best level and delivering consistent results. I attended coaching session from her and during the session she really helped me deep dive into the topic. She navigated with me very well in terms of finding out what I was really looking for and concluded the session with right action plan."
+        subheading:"DATA SCIENTIST, SCHNEIDER ELECTRIC, BENGALURU",
+        value:"I have been connected with Gunjan for quite a few months now, my interaction with her has always been empowering and positive. Her commitment, eagerness to deliver, and continuous learning makes her stand apart from the crowd. She has capabilities and discipline to use her knowledge at the best level and delivering consistent results. During the coaching session she really helped me deep dive into the topic. She navigated with me very well in terms of finding out what I was really looking for and concluded the session with right action plan."
     },{
+        pic:VD,
         heading:"VIVEK DICHOLKAR",
         subheading:"ASSOCIATE DIRECTOR, COGNIZANT ATLANTA, US",
         value:"Gunjan delivered an engaging and inspiring webinar Resiliency Spectrum. Her energetic approach reinforces learning and provides a new set of tools to bounce back. Her articles on Linkedin are the perfect blend of positive mindset, thoughtfulness, and creativity. What stands out about Gunjan is that she is open minded, listens well and has the ability to empathize"
     },{
+        pic:youth,
         heading:"YOUTH COACHING",
         subheading:"CHENNAI",
         value:"Gunjan has not only been a life coach for me, but a mentor, a friend and a role model. Whenever I talk to her, I just feel like I'm talking to one of my closet friends. I have learnt so many things about myself that I've never ever known before. I've personally felt a lot better and confident about myself after every session and I cannot thank Gunjan enough! I just want to say that she is a beautiful person inside out who inspires everyone she comes across! Thank you so so much for this Gunjan!! "
